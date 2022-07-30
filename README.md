14bbbae354f275b6e02e6a9a4aa163c16cb11ecf
sourabh sardesai <sourabh_sardesai@outlook.com>
2022-07-30 19:57:32 +0100

    added comments and exception handling

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # sprout
 Apis for integrating with ML model
+This project contains 3 apis with 2 apis exposed for the external use and one internal api. 
+apis - 
+1] http://localhost:5000 [external api]
+2] http://localhost:5000/posts [external api]
+3] http://localhost:5000/sentences [internal api]
+
+1] http://localhost:5000 [external api] - 
+This api can be used to get all records stored in the DB/list
+
+2] http://localhost:5000/posts [external api]
+This api allows user to post json object as shown below, which will allow user to post paragraaph or sentences to find the sentiment or to find the foul language.
+curl -X 'POST' \
+'http://127.0.0.1:5000/posts/' \
+-H 'accept: application/json' \
+-H 'Content-Type: application/json' \
+-d '{
+"title": "This is an engaging title",
+"paragraphs": [
+"This is the first paragraph. It contains two sentences.",
+"This is the second parapgraph. It contains two more sentences",
+"Third paraphraph here."
+]
+}'
+
+the response of the above apis is as follows - 
+[{"hasFoulLanguage": true, "sentence": {"fragment": "This is the first paragraph. It contains two sentences."}}, {"hasFoulLanguage": false, "sentence": {"fragment": "This is the second parapgraph. It contains two more sentences"}}, {"hasFoulLanguage": false, "sentence": {"fragment": "Third paraphraph here."}}]
+
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+#Initializing of the project, this file contains all the api declarations
 #!/usr/bin/env python
 # encoding: utf-8
 import http
@@ -6,24 +8,33 @@ from flask import Flask , request , jsonify
 from services import ml_service
 DB = []
 app = Flask(__name__)
+# This api gets all the data records stored in the DB/list
 @app.route('/')
 def index():
-
-    return json.dumps(ml_service.get_all())
-
+    try:
+        return json.dumps(ml_service.get_all())
+    except:
+        return json.dumps({"response":"something went wrong"})
+# This is the internal api which accepts a single sentence and returns the foul sentiment as the response.
 @app.route('/sentences', methods = ['POST'])
 def ml_service_caller():
-    print(request.data)
-    respone = json.loads(request.data)
-    final_res = ml_service.detect_senti(respone)
-    return final_res
-
+    try:
+        print(request.data)
+        respone = json.loads(request.data)
+        final_res = ml_service.detect_senti(respone)
+        return final_res
+    except:
+        return json.dumps({"response": "something went wrong"})
+# This api accepts the posts and calls an internal api within to give sentiment to the user
 @app.route('/posts', methods = ['POST'])
 def posts():
     #print(request.data)
-    response = json.loads(request.data)
-    final_res = ml_service.foul_lang(response)
-    return final_res
+    try:
+        response = json.loads(request.data)
+        final_res = ml_service.foul_lang(response)
+        return final_res
+    except:
+        return json.dumps({"response":"something went wrong"})
 
 
 
